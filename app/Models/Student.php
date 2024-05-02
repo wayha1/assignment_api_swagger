6b0668f7af4cc4c32a94be8e81651574e4093dc5
JackyDom <sopheaoudom8@gmail.com>
2024-05-02 20:55:01 +0700

    update

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,8 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
-    protected $table = 'student_info';
-    protected $guarded = [];
-    protected $fillable = ['name', 'email'];
+    // protected $table = 'student_info';
+    // protected $guarded = [];
+    // protected $fillable = ['name', 'email'];
     
 }
